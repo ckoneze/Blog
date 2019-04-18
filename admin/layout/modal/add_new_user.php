6e7b4e12f03e14784a552e84730f113dddf44a35
ckoneze <neckozenica@gmail.com>
2019-04-18 10:39:53 +0200

    18-04-2019-10:40

--- admin/layout/modal/add_new_user.php
+++ admin/layout/modal/add_new_user.php
@@ -5,13 +5,14 @@
         $add_user_name=$_POST['user_name'];
         $add_user_username=$_POST['user_username'];
         $add_user_email=$_POST['user_email'];
+        $add_user_password=$_POST['user_password'];
         $add_user_gender=$_POST['user_gender'];
         $add_user_image=$_POST['user_image'];
         $add_user_code=$_POST['user_code'];
         $add_user_status=$_POST['user_status'];
         $add_user_type=$_POST['user_type'];
 
-        $sql_add_user = "INSERT INTO users(name,username,email,gender,image,code,status,type) VALUES('$add_user_name', '$add_user_username', '$add_user_email', '$add_user_gender', '$add_user_image', '$add_user_code', '$add_user_status', '$add_user_type')";
+        $sql_add_user = "INSERT INTO users(name,username,email,password,gender,image,code,status,type) VALUES('$add_user_name', '$add_user_username', '$add_user_email', '$add_user_password', '$add_user_gender', '$add_user_image', '$add_user_code', '$add_user_status', '$add_user_type')";
         $result_sql_add_user= mysqli_query($dbconnection, $sql_add_user);
         if (!$result_sql_add_user)
                 {
@@ -49,6 +50,11 @@
                 <div class="form-group col-md-6">
                   <label for="cat_title" class="col-form-label"> Email:</label>
                   <input type="text" class="form-control" id="user_email" name="user_email" placeholder="Enter Email Here" required="">
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="cat_title" class="col-form-label"> Gender:</label><br>
+                  <label><input type="radio" name="optradio" checked>Option 1</label>
+                  <label><input type="radio" name="optradio" checked>Option 1</label>
                 </div>
                 <div class="form-group">
                   <label for="cat_desc" class="col-form-label">Description:</label>
